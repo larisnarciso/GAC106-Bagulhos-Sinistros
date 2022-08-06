9e8764f8921bd7b607bffbc915f526acdcc4db6c
Ana Torres <anatorrescodes@gmail.com>
2022-08-06 19:08:20 -0300

    Criar classe de Item não Coletavel

---
+++ src/br/ufla/gac106/s2022_1/bagulhosSinistros/item/ItemNaoColetavel.java
@@ -0,0 +1,7 @@
+package br.ufla.gac106.s2022_1.bagulhosSinistros.item;
+
+public class ItemNaoColetavel extends Item {
+    public ItemNaoColetavel(String nome, String descricao) {
+        super(nome, descricao, false);
+    }  
+}
