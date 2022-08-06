13a247fb7b0b0cd7d6b205b3ee300b4484137526
Ana Torres <anatorrescodes@gmail.com>
2022-08-06 19:08:03 -0300

    Criar classe de Item Coletavel

---
+++ src/br/ufla/gac106/s2022_1/bagulhosSinistros/item/ItemColetavel.java
@@ -0,0 +1,7 @@
+package br.ufla.gac106.s2022_1.bagulhosSinistros.item;
+
+public class ItemColetavel extends Item {
+    public ItemColetavel(String nome, String descricao) {
+        super(nome, descricao, true);
+    }  
+}
