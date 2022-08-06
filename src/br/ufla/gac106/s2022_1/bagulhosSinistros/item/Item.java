a325a023aefe8889952f6bff1eb9a0d3156b5901
Ana Torres <anatorrescodes@gmail.com>
2022-08-06 19:08:56 -0300

    Aterando classe Item para melhor herança de classes

--- src/br/ufla/gac106/s2022_1/bagulhosSinistros/item/Item.java
+++ src/br/ufla/gac106/s2022_1/bagulhosSinistros/item/Item.java
@@ -3,10 +3,12 @@ package br.ufla.gac106.s2022_1.bagulhosSinistros.item;
 public class Item {
     private String nome;
     private String descricao;
+    private boolean ehColetavel;
 
-    public Item(String nome, String descricao) {
+    public Item(String nome, String descricao, boolean ehColetavel) {
         this.nome = nome;
         this.descricao = descricao;
+        this.ehColetavel = ehColetavel;
     }
 
     /**
@@ -21,5 +23,12 @@ public class Item {
      */
     public String getDescricao() {
         return descricao;
+    }
+    
+    /**
+     * @return O tipo do item.
+     */
+    public boolean getEhColetavel() {
+        return ehColetavel;
     }
 }
