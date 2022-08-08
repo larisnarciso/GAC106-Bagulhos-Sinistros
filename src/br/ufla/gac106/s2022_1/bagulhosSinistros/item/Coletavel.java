3ce2f6c741603387b478922628dc6d0dec9f2ac0
Ana Torres <anatorrescodes@gmail.com>
2022-08-08 20:49:18 -0300

    Arterar item e suas subclasses

---
+++ src/br/ufla/gac106/s2022_1/bagulhosSinistros/item/Coletavel.java
@@ -0,0 +1,7 @@
+package br.ufla.gac106.s2022_1.bagulhosSinistros.item;
+
+public class Coletavel extends Item {
+    public Coletavel(String nome, String descricao, String acao) {
+        super(nome, descricao, acao, true);
+    }
+}
