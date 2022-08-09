afba00b24788f7c873a43dd8f3a1f2038fdc9f98
Ana Torres <anatorrescodes@gmail.com>
2022-08-09 15:28:20 -0300

    Alterar nome da pasta de Itens

--- src/br/ufla/gac106/s2022_1/bagulhosSinistros/Itens/Pista.java
+++ src/br/ufla/gac106/s2022_1/bagulhosSinistros/Itens/Pista.java
@@ -1,4 +1,4 @@
-package br.ufla.gac106.s2022_1.bagulhosSinistros.item;
+package br.ufla.gac106.s2022_1.bagulhosSinistros.Itens;
 
 public class Pista extends Item {
     public Pista(String nome, String descricao) {
